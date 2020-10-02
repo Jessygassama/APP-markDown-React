85f230b8fbff5ed458bdad5fe5745edcb8d40535
jessygassama <jessygassama@gmail.com>
2020-10-02 15:10:03 +0200

    projet final

---
+++ precache-manifest.d35ba7d3f41f03e86410cdc434074632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854a449d4a66d52854f69e1d5f0a7d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f3d1f7c7d4d751b6863",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5593495b940fdaa08a59",
+    "url": "/static/css/main.d56770a5.chunk.css"
+  },
+  {
+    "revision": "7f3d1f7c7d4d751b6863",
+    "url": "/static/js/2.5879a3ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5879a3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5593495b940fdaa08a59",
+    "url": "/static/js/main.d6c675ed.chunk.js"
+  },
+  {
+    "revision": "dfaea58438c29b5c4cf1",
+    "url": "/static/js/runtime-main.5ce527b0.js"
+  }
+]);
